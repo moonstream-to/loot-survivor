7fe452a3a129d3ed6e102dcf9875f838a0cca6fc
Neeraj Kashyap <nkashy1@gmail.com>
2023-10-31 14:09:36 -0700

    Added json annotation to ParsedEvent

--- parse-events.go
+++ parse-events.go
@@ -50,9 +50,8 @@ var ErrParsingStats error = errors.New("could not parse stats")
 var ErrParsingCombatSpec error = errors.New("could not parse combat spec")
 
 type ParsedEvent struct {
-	Name string `json:"name"`
-	// TODO: Add json annotation
-	Event interface{}
+	Name  string      `json:"name"`
+	Event interface{} `json:"event"`
 }
 
 var EVENT_UNKNOWN string = "UNKNOWN"
